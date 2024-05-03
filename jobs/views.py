a8c4e0c99e2446d9a297dc4d5731f815dc46600e
wayne71 <wayne.hatter1971@gmail.com>
2024-05-03 18:20:14 -0500

    creating views in django

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Jobs
 
 def wayne(request):
@@ -6,5 +6,5 @@ def wayne(request):
     return render(request, 'jobs/home.html', {'jobs':jobs})
 
 def detail(request, job_id):
-    print(job_id)
-    return render(request, 'jobs/home.html')
+    job_detail = get_object_or_404(Jobs, pk=job_id)
+    return render(request, 'jobs/detail.html', {'job':job_detail})
